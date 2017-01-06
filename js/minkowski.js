077463e6741b1cba7ca50eb7d5a13bd4bc828687
Chris Uehlinger <chris.uehlinger@gmail.com>
2017-01-06 01:55:37 -0500

    Implement black hole curvature using Gullstrand-Painlevé coordinates

--- js/minkowski.js
+++ js/minkowski.js
@@ -19,32 +19,33 @@ var timeLimit = 100,
 
 var useRelativity = true,
     useLorentzBoost = false,
+    useBlackHoles = true,
     showLightCones = true;
 
 var player = {
-        absolutePosition: 0,
-        relativePosition: 0,
-        mass: 100,
-        thrust: 10,
-        velocity: 0.5,
-        color: [0,0, 255],
-        size: 5,
-        reference: useRelativity
-    },
-    others = lodash.fill(Array(1), 0).map(function(){
-        var pos = 9;//aMath.random()*20 - 10;
+    absolutePosition: 0,
+    relativePosition: 0,
+    mass: 100,
+    thrust: 10,
+    velocity: 0,
+    color: [0, 0, 255],
+    size: 5,
+    reference: useRelativity
+},
+    others = lodash.fill(Array(1), 0).map(function () {
+        var pos = -1;//aMath.random()*20 - 10;
         return {
             absolutePosition: pos,
             relativePosition: pos,
-            velocity: 0.5,
+            velocity: -0.5,
             color: [100, 0, 0],
             size: 4,
             reference: false
         };
     }),
     hmm = -10,
-    events = lodash.fill(Array(0), 0).map(function(){
-        var pos = [Math.random()*40 - 20, Math.random()*60 - 10];
+    events = lodash.fill(Array(0), 0).map(function () {
+        var pos = [Math.random() * 40 - 20, Math.random() * 60 - 10];
         return {
             absolutePosition: pos,
             relativePosition: pos,
@@ -52,8 +53,8 @@ var player = {
             color: [0, 100, 0],
             size: 4
         };
-    }).concat(lodash.fill(Array(100), 0).map(function(){
-        var pos = [0.5*hmm+4, hmm];
+    }).concat(lodash.fill(Array(100), 0).map(function () {
+        var pos = [0.5 * hmm + 4, hmm];
         hmm++;
         return {
             absolutePosition: pos,
@@ -66,7 +67,7 @@ var player = {
     blackHole = {
         absolutePosition: -15,
         relativePosition: -15,
-        radius: 1,
+        radius: 5,
         color: [0, 0, 0],
         size: 4
     },
@@ -80,14 +81,14 @@ console.log(events)
 var present;
 
 initDiagram(objectCount, eventCount);
-setTimeout(function(){
+setTimeout(function () {
     timerStarted = true;
     initSimulation();
 }, 1000);
 
 var thrustSign = 0;
 window.onkeydown = function (e) {
-    if(timerEnded) {
+    if (timerEnded) {
         return;
     }
 
@@ -102,7 +103,7 @@ window.onkeydown = function (e) {
             break;
         case 76:
             objects.push({
-                absolutePosition: objects[0].absolutePosition,
+                absolutePosition: objects[1].absolutePosition,
                 relativePosition: 0,
                 velocity: 1,
                 color: [100, 0, 100],
@@ -110,7 +111,7 @@ window.onkeydown = function (e) {
                 reference: false
             });
             objects.push({
-                absolutePosition: objects[0].absolutePosition,
+                absolutePosition: objects[1].absolutePosition,
                 relativePosition: 0,
                 velocity: -1,
                 color: [100, 0, 100],
@@ -127,77 +128,89 @@ window.onkeydown = function (e) {
 }
 
 
-window.onkeyup = function(){
+window.onkeyup = function () {
     thrustSign = 0;
 }
 
-function initSimulation(){
+function initSimulation() {
     var lastFrameTime = startTime = Date.now();
     var updateFrame = setTimeout(update, 50);
-    function update(){
+    function update() {
         var timeSinceLastFrame = (Date.now() - lastFrameTime) / 1000;
         timeElapsed = (Date.now() - startTime) / 1000;
         lastFrameTime = Date.now();
 
-        var referenceFrame = objects.filter(function(obj) { return obj.reference; })[0] || {
+        var referenceFrame = objects.filter(function (obj) { return obj.reference; })[0] || {
             absolutePosition: 0,
             velocity: 0,
             thrust: 0,
             mass: 1
         }
 
-        var gamma = Math.sqrt(1 - referenceFrame.velocity*referenceFrame.velocity);
+        var gamma = Math.sqrt(1 - referenceFrame.velocity * referenceFrame.velocity);
         var relThrust = referenceFrame.thrust / (gamma * referenceFrame.mass);
         referenceFrame.velocity += thrustSign * relThrust * timeSinceLastFrame;
         referenceFrame.velocity = Math.max(Math.min(referenceFrame.velocity, 0.99999), -0.99999);
-        gamma = Math.sqrt(1 - referenceFrame.velocity*referenceFrame.velocity);
+        gamma = Math.sqrt(1 - referenceFrame.velocity * referenceFrame.velocity);
 
         referenceFrame.absolutePosition += referenceFrame.velocity * timeSinceLastFrame;
 
-        referenceFrame.absolutePosition
+        // console.log('Y = ' + Math.round(gamma*1000)/1000 + ' x = ' + Math.round(referenceFrame.absolutePosition*1000)/1000 + ' v = ' + Math.round(referenceFrame.velocity*1000)/1000 + 'c');
 
-        console.log('Y = ' + Math.round(gamma*1000)/1000 + ' x = ' + Math.round(referenceFrame.absolutePosition*1000)/1000 + ' v = ' + Math.round(referenceFrame.velocity*1000)/1000 + 'c');
 
-        objects.map(function(object){
-            if(!object.reference) {
-                object.absolutePosition += object.velocity * timeSinceLastFrame;
+        blackHoles.map(function (object) {
+            object.relativePosition = (object.absolutePosition - referenceFrame.absolutePosition);
+        });
+
+        objects.map(function (object) {
+            if (!object.reference) {
+                var t = 1;
+                if (useBlackHoles) {
+                    // Calculated using Gullstrand-Painlevé coordinates
+                    var r = Math.abs(object.absolutePosition - blackHole.absolutePosition);
+                    t = 1 - Math.sqrt(blackHole.radius/r);
+                }
+
+                object.absolutePosition += object.velocity * timeSinceLastFrame * t;
                 object.relativePosition = object.absolutePosition - referenceFrame.absolutePosition;
 
-                if (useLorentzBoost){
-                    var relativeVelocity = (referenceFrame.velocity - object.velocity) / (1 - referenceFrame.velocity*object.velocity);
-                    var relGamma = Math.sqrt(1 - relativeVelocity*relativeVelocity) || 1;
+                if (useLorentzBoost) {
+                    var relativeVelocity = (referenceFrame.velocity - object.velocity) / (1 - referenceFrame.velocity * object.velocity);
+                    var relGamma = Math.sqrt(1 - relativeVelocity * relativeVelocity) || 1;
                     object.relativePosition = relGamma * object.relativePosition;
                 }
+                var r = object.relativePosition - blackHole.relativePosition;
+
             }
         });
 
-        blackHoles.map(function(object){
-            object.relativePosition = (object.absolutePosition - referenceFrame.absolutePosition);
-        });
-        
-        events.map(function(event){
+        events.map(function (event) {
+            // Galillean Relativity
             event.relativePosition = [
                 event.absolutePosition[0] - referenceFrame.absolutePosition,
                 event.absolutePosition[1] - timeElapsed
             ];
-            if(useLorentzBoost) {
+
+            // Special Relativity
+            if (useLorentzBoost) {
                 event.relativePosition = [
-                    gamma*(event.relativePosition[0] - referenceFrame.velocity*event.relativePosition[1]),
-                    gamma*(event.relativePosition[1] - referenceFrame.velocity*event.relativePosition[0])
+                    gamma * (event.relativePosition[0] - referenceFrame.velocity * event.relativePosition[1]),
+                    gamma * (event.relativePosition[1] - referenceFrame.velocity * event.relativePosition[0])
                 ];
             }
+
         });
 
-        events = events.filter(function(event){
+        events = events.filter(function (event) {
             return event
         });
 
         updateFrame = setTimeout(update, 50);
     }
 
-    setInterval(function(){
+    setInterval(function () {
         timeElapsed = (Date.now() - startTime) / 1000;
-        var pos = [Math.random()*20-10, Math.random()*10];
+        var pos = [Math.random() * 20 - 10, Math.random() * 10];
         // events.push({
         //     absolutePosition: pos,
         //     relativePosition: pos,
@@ -206,10 +219,10 @@ function initSimulation(){
         //     size: 4
         // });
 
-        objects.map(function(object){
+        objects.map(function (object) {
             events.push({
                 absolutePosition: [object.absolutePosition, timeElapsed],
-                relativePosition: [0,0],
+                relativePosition: [0, 0],
                 velocity: 0,
                 color: object.color,
                 size: object.size
@@ -217,16 +230,16 @@ function initSimulation(){
         });
 
         update();
-    }, 1000);
+    }, 500);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log('ADVANCE');
         clearTimeout(updateFrame);
         timerEnded = true;
     }, timeLimit * 1000);
 }
 
-function initDiagram(){    
+function initDiagram() {
     var view = mathbox
         .set({
             focus: 3,
@@ -236,71 +249,71 @@ function initDiagram(){
             scale: [1.5, 1.5, 1.5],
         });
     view
-    .axis({
-        detail: 1,
-    })
-    .scale({
-        divide: 1,
-    })
-    .ticks({
-        classes: ['foo', 'bar'],
-        width: 2
-    })
-    .grid({
-        divideX: 2*stRadius,
-        divideY: 2*stRadius,
-        width: 1,
-        opacity: 0.5,
-        zBias: -5,
-    })
-    .array({
-        id: 'currentPosition',
-        width: 50,
-        expr: function(emit, i, t){
-            if(i < objects.length){
-                emit(objects[i].relativePosition);
-            }
-        },
-        channels: 1
-    })
-    .array({
-        id:'objectColors',
-        width: 50,
-        channels: 4,
-        expr: function(emit, i, t){
-            if(i < objects.length){
-                var color = objects[i].color;
-                emit(color[0], color[1], color[2], 1.0);
-            }
-        },
-    })
-    .array({
-        id:'objectSizes',
-        width: 50,
-        channels: 1,
-        expr: function(emit, i, t){
-            if(i < objects.length) {
-                emit(objects[i].size);
-            }
-        },
-    })
-    .point({
-        points: '#currentPosition',
-        // color: 0x3090FF,
-        colors:'#objectColors',
-        // size: 10
-        sizes: "#objectSizes"
-    })
-    .axis({
-        axis: 2
-    });
+        .axis({
+            detail: 1,
+        })
+        .scale({
+            divide: 1,
+        })
+        .ticks({
+            classes: ['foo', 'bar'],
+            width: 2
+        })
+        .grid({
+            divideX: 2 * stRadius,
+            divideY: 2 * stRadius,
+            width: 1,
+            opacity: 0.5,
+            zBias: -5,
+        })
+        .array({
+            id: 'currentPosition',
+            width: 50,
+            expr: function (emit, i, t) {
+                if (i < objects.length) {
+                    emit(objects[i].relativePosition);
+                }
+            },
+            channels: 1
+        })
+        .array({
+            id: 'objectColors',
+            width: 50,
+            channels: 4,
+            expr: function (emit, i, t) {
+                if (i < objects.length) {
+                    var color = objects[i].color;
+                    emit(color[0], color[1], color[2], 1.0);
+                }
+            },
+        })
+        .array({
+            id: 'objectSizes',
+            width: 50,
+            channels: 1,
+            expr: function (emit, i, t) {
+                if (i < objects.length) {
+                    emit(objects[i].size);
+                }
+            },
+        })
+        .point({
+            points: '#currentPosition',
+            // color: 0x3090FF,
+            colors: '#objectColors',
+            // size: 10
+            sizes: "#objectSizes"
+        })
+        .axis({
+            axis: 2
+        });
 
     view.array({
         id: 'events',
         channels: 2,
         width: 1e4,
-        expr:function(emit, i, t){
-            if(i < events.length &&
+        expr: function (emit, i, t) {
+            if (i < events.length &&
                 Math.abs(events[i].relativePosition[0]) < stRadius &&
                 Math.abs(events[i].relativePosition[1]) < stRadius) {
                 emit(events[i].relativePosition[0], events[i].relativePosition[1]);
@@ -310,9 +323,9 @@ function initDiagram(){
         id: 'eventColors',
         width: 1e4,
         channels: 4,
-        expr:function(emit, i, t){
-            if(i < events.length) {
-                if(i < events.length &&
+        expr: function (emit, i, t) {
+            if (i < events.length) {
+                if (i < events.length &&
                     Math.abs(events[i].relativePosition[0]) < stRadius &&
                     Math.abs(events[i].relativePosition[1]) < stRadius) {
                     var color = events[i].color;
@@ -323,7 +336,7 @@ function initDiagram(){
     }).point({
         points: '#events',
         // color: 0x3090FF,
-        colors:'#eventColors',
+        colors: '#eventColors',
         size: 10
     })
 
@@ -334,8 +347,8 @@ function initDiagram(){
         channels: 1,
         items: 3,
         width: 10,
-        expr:function(emit, i, t){
-            if(i < blackHoles.length){
+        expr: function (emit, i, t) {
+            if (i < blackHoles.length) {
                 // Math.abs(events[i].relativePosition[0]) < stRadius &&
                 // Math.abs(events[i].relativePosition[1]) < stRadius) {
                 emit(blackHoles[i].relativePosition);
@@ -353,13 +366,13 @@ function initDiagram(){
         width: 2,
         items: 2,
         channels: 2,
-        live:false,
-        data:[
-            [-stRadius, -stRadius], [-stRadius,stRadius],
+        live: false,
+        data: [
+            [-stRadius, -stRadius], [-stRadius, stRadius],
             [stRadius, stRadius], [stRadius, -stRadius]
         ]
     }).line({
-        color:[100,0,100],
+        color: [100, 0, 100],
         width: 1,
         visible: showLightCones,
         end: false
