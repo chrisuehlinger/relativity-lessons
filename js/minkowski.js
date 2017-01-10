6fc6b7ccb2553f7ed094d2b431112583a2ba9026
Chris Uehlinger <chris.uehlinger@gmail.com>
2017-01-10 12:35:34 -0500

    Add stats.js as a bower dependency

--- js/minkowski.js
+++ js/minkowski.js
@@ -1,3 +1,13 @@
+var stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
+function showStats() {
+    stats.update();
+    requestAnimationFrame(showStats);
+}
+requestAnimationFrame(showStats);
+
 lodash = _;
 _.noConflict();
 
